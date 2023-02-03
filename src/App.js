f7aca2be0290a515d3cee36bc0bb3c1b6bb686ae
Darasimi Oloyede <darasimioloyede@gmail.com>
2023-02-03 16:37:49 +0100

    Joined backend & frontend and finished build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
+import axios from './axios.js';
+
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  
+  useEffect(()=>
+  {
+    axios.get('/messages/sync').then(response =>{
+      console.log(response.data);
+      setMessages(response.data);
+
+    })
+  },[]);
+  console.log(messages);
+  
+
+
+
+
+  useEffect(()=>{
+    const pusher = new Pusher('3fffe69bddee4216f2fa', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+  },[]);
+
   return (
     <div className="app">
       <div className='app__body'>
